7c9e74f7208602be58d0fccdbc6897113865f370
Andrew <andrewsmith1025@gmail.com>
2021-10-18 14:20:48 -0400

    transformer work

--- scripts/utils.py
+++ scripts/utils.py
@@ -23,7 +23,7 @@ from os import path
 import tensorflow as tf
 from tensorflow.keras.callbacks import TensorBoard
 from time import time
-
+from tensorflow.keras import layers
 from tensorflow import keras
 
 from tensorflow.keras.callbacks import TensorBoard
@@ -334,3 +334,43 @@ def split_and_shuffle(df):
     print('Weight for class 2: {:.2f}'.format(weight_for_w))
 
     return train_features,train_labels,val_features,val_labels,test_features,test_labels,class_weight
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Attention and Normalization
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(inputs, inputs)
+    x = layers.Dropout(dropout)(x)
+    x = layers.LayerNormalization(epsilon=1e-6)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.Dense(units=128,activation="relu")(res)
+    # x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(res)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Dense(units=128,activation="relu")(res)
+
+    # x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    x = layers.LayerNormalization(epsilon=1e-6)(x)
+    return x + res
+def build_model(
+    input_shape,
+    head_size,
+    num_heads,
+    ff_dim,
+    num_transformer_blocks,
+    mlp_units,
+    n_classes,
+    dropout=0,
+    mlp_dropout=0,
+):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(n_classes, activation="softmax")(x)
+    return keras.Model(inputs, outputs)
--- transformer.py
+++ transformer.py
@@ -41,42 +41,7 @@ y_train = y_train[idx]
 
 from tensorflow import keras
 from tensorflow.keras import layers
-def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
-    # Attention and Normalization
-    x = layers.MultiHeadAttention(
-        key_dim=head_size, num_heads=num_heads, dropout=dropout
-    )(inputs, inputs)
-    x = layers.Dropout(dropout)(x)
-    x = layers.LayerNormalization(epsilon=1e-6)(x)
-    res = x + inputs
 
-    # Feed Forward Part
-    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(res)
-    x = layers.Dropout(dropout)(x)
-    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
-    x = layers.LayerNormalization(epsilon=1e-6)(x)
-    return x + res
-def build_model(
-    input_shape,
-    head_size,
-    num_heads,
-    ff_dim,
-    num_transformer_blocks,
-    mlp_units,
-    dropout=0,
-    mlp_dropout=0,
-):
-    inputs = keras.Input(shape=input_shape)
-    x = inputs
-    for _ in range(num_transformer_blocks):
-        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
-
-    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
-    for dim in mlp_units:
-        x = layers.Dense(dim, activation="relu")(x)
-        x = layers.Dropout(mlp_dropout)(x)
-    outputs = layers.Dense(n_classes, activation="softmax")(x)
-    return keras.Model(inputs, outputs)
 
 input_shape = x_train.shape[1:]
 
@@ -87,6 +52,7 @@ model = build_model(
     ff_dim=4,
     num_transformer_blocks=4,
     mlp_units=[128],
+    n_classes=n_classes,
     mlp_dropout=0.4,
     dropout=0.25
 )
@@ -111,7 +77,7 @@ model.fit(
     x_train,
     y_train,
     validation_data=(x_val,y_val),
-    epochs=2,
+    epochs=200,
     batch_size=64,
     callbacks=callbacks,
 )
