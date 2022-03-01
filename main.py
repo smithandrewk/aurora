64ed4b4980c03d4e9ec8c8fee37107ca5bdd148e
Andrew <aks3@email.sc.edu>
2022-03-01 14:46:15 -0500

    initial work on modules

---
+++ main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+"""
+Pipeline for ...
+"""
+print(__doc__)
+from scripts.modules import *
+
+preprocess()
+window()
+scale()
---
+++ scripts/modules.py
@@ -0,0 +1,30 @@
+def preprocess():
+    """
+    preprocess preprocesses.
+
+    @params
+        filename : name of file
+    """
+    print("Starting Preprocessing")
+
+    print("Finishing Preprocessing")
+def window():
+    """
+    window windows.
+
+    @params
+        filename : name of file
+    """
+    print("Starting Windowing")
+
+    print("Finishing Windowing")
+def scale():
+    """
+    scale scales.
+
+    @params
+        filename : name of file
+    """
+    print("Starting Scaling")
+
+    print("Finishing Scaling")
