7ab5c266e9c34ff053f73ab38f3cb30e4b99ef26
Andrew <aks3@email.sc.edu>
2022-03-24 13:29:05 -0400

    Move select features to modules

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
 from scripts.modules import *
-# rename_data_in_raw()
-# preprocess_renamed_files()
-# fix_anomalies_in_preprocessed_files()
-# def select_features():
-#     from os import listdir
-#     from pandas import read_csv,DataFrame
-#     dir = f'data/preprocessed'
-#     for file in listdir(dir):
-#         print(dir,file)
-#         df = read_csv(f'{dir}/{file}')
-#         Y = DataFrame()
-#         if(df.columns[0]!="Class"):
-#             print(f'Not scored, skipping')
-#             continue
-#         # Remove Unwanted Features
-#         # df = df[['Class','EEG 2']]
-#         # df = df[['Class','EEG 2','Activity']]
-#         df = df.drop(columns=['Activity'])
-#         df.to_csv(f'{dir}/{file}',index=False)
-# select_features()
+rename_data_in_raw()
+preprocess_renamed_files()
+fix_anomalies_in_preprocessed_files()
+select_features()
 window_preprocessed_files()
 balance_windowed_files()
 concatenate_balanced_files()
