2913e1df2f90de66a39e54341b0c0cb156342ddc
MMAZeem3 <musa.mazeem@gmail.com>
2022-03-24 13:54:39 -0400

    fixes

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from scripts.modules import *
 create_time_dir()
 
 ## Run these only when we have new data
-rename_data_in_raw()
-preprocess_renamed_files()
+# rename_data_in_raw()
+# preprocess_renamed_files()
 # fix_anomalies_in_preprocessed_files()
-window_preprocessed_files()
-balance_windowed_files()
-concatenate_balanced_files()
+# window_preprocessed_files()
+# balance_windowed_files()
+# concatenate_balanced_files()
 
 ## Run these to train new model
-hln,baseline_history = load_data_and_train_model(dir)
-baseline_results = load_data_and_test_model(hln, dir)
-
-#'03.23.2022_10:54'
+# Pass directory name (eg. '03.24.2022_12:40') to load_data_and_train_model() and load_data_and_test_model() to use data from a previous session
+hln,baseline_history = load_data_and_train_model('03.24.2022_12:40')   
+baseline_results = load_data_and_test_model(hln, '03.24.2022_12:40')
