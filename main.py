b3f204bb6b27dfa89251f5dd82e12bfa34cf9f77
Andrew <aks3@email.sc.edu>
2022-03-24 13:28:40 -0400

    Add select features function

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from scripts.modules import *
 # rename_data_in_raw()
 # preprocess_renamed_files()
 # fix_anomalies_in_preprocessed_files()
-# window_preprocessed_files()
-# balance_windowed_files()
-# concatenate_balanced_files()
-# split_and_shuffle('X.csv')
+# def select_features():
+#     from os import listdir
+#     from pandas import read_csv,DataFrame
+#     dir = f'data/preprocessed'
+#     for file in listdir(dir):
+#         print(dir,file)
+#         df = read_csv(f'{dir}/{file}')
+#         Y = DataFrame()
+#         if(df.columns[0]!="Class"):
+#             print(f'Not scored, skipping')
+#             continue
+#         # Remove Unwanted Features
+#         # df = df[['Class','EEG 2']]
+#         # df = df[['Class','EEG 2','Activity']]
+#         df = df.drop(columns=['Activity'])
+#         df.to_csv(f'{dir}/{file}',index=False)
+# select_features()
+window_preprocessed_files()
+balance_windowed_files()
+concatenate_balanced_files()
+split_and_shuffle('X.csv')
 hln,baseline_history = load_data_and_train_model()
-baseline_results = load_data_and_test_model(hln)
+baseline_results = load_data_and_test_model(hln,baseline_history)
