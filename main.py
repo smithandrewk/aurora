e64f1c4f3e3152dd9569a4c70dbc92c884110e20
MMAZeem3 <musa.mazeem@gmail.com>
2022-06-03 15:24:43 -0400

    finished new zdb code, ready for application

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ Pipeline for ...
 print(__doc__)
 from lib.modules import *
 
-# args = create_and_check_args()
+args = create_and_check_args()
 
-# rename_data_in_raw()
-# preprocess_data_in_renamed()
-# scale_features_in_preprocessed()
-# window_and_score_files_in_scaled()
-# remap_names_lstm(args.ann_model)
+rename_data_in_raw()
+preprocess_data_in_renamed()
+scale_features_in_preprocessed()
+window_and_score_files_in_scaled()
+remap_names_lstm(args.ann_model)
 
 rename_files_in_raw_zdb()
 score_files_in_renamed_zdb()
