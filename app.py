8577a89d6c6fb71dd12433f5e122466e268e211c
Andrew <andrewsmith1025@gmail.com>
2022-06-07 17:14:14 -0400

    notes

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def index():
 @app.route('/dashboard')
 @login_required
 def dashboard():
+    print(current_user.email)
     logs = list(ScoringLog.query.filter_by(email=current_user.email, is_deleted=False))
     logs.reverse()
     print(logs)
@@ -127,6 +128,33 @@ def score_data():
                                     filename=filename))
     return render_template('score-data.jinja', form=form,name=f'{current_user.first_name} {current_user.last_name}')
 
+@app.route('/notes', methods=['GET', 'POST'])
+@login_required
+def notes():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            from datetime import datetime
+            date = datetime.now()
+            print({'title': title, 'content': content})
+            note = Notes(email=current_user.email, 
+                             note_name=title,
+                             date_written=date,
+                             contents=content)
+            db.session.add(note)
+            db.session.commit()
+            return redirect(url_for('notes'))
+
+    notes = list(Notes.query.filter_by(email=current_user.email))
+    notes.reverse()
+    return render_template('notes.jinja', 
+                           name=f'{current_user.first_name} {current_user.last_name}',notes=notes)
 @app.route('/process-file/<model>/<int:iszip>/<filename>', methods=['GET', 'POST'])
 @login_required
 def process_file(model, iszip, filename):
@@ -257,7 +285,15 @@ class ScoringLog(db.Model):
     filename = db.Column(db.String(200), nullable=False)    #filename in ARCHIVE_FOLDER
     model = db.Column(db.String(200), nullable=False)
     files = db.Column(db.String(1000), nullable=False)      # json list of files
-    is_deleted = db.Column(db.Boolean, default=False)  
+    is_deleted = db.Column(db.Boolean, default=False)
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+    note_name = db.Column(db.String(200), nullable=False)
+    date_written = db.Column(db.DateTime, default=datetime.utcnow)
+    contents = db.Column(db.String(1000), nullable=False)      # json list of files
+    is_deleted = db.Column(db.Boolean, default=False)
 
 if __name__=='__main__':
     init_dir(db)
