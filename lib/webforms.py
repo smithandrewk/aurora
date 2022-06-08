42a9c8ac2970de78603f17fcf5a245a15aecfeb9
MMAZeem3 <musa.mazeem@gmail.com>
2022-06-08 11:50:41 -0400

    removed route 'score_data' and related code/files

--- lib/webforms.py
+++ lib/webforms.py
@@ -15,12 +15,6 @@ class SignupForm(FlaskForm):
     password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Create User")
 
-class FileUploadForm(FlaskForm):
-    model = SelectField("Choose a Model", validators=[DataRequired()], validate_choice=False)
-    iszip = SelectField("Choose upload type", choices=[(1, 'Zip Archive'), (0, 'Individual File')], validators=[DataRequired()])
-    file_submission = FileField("Select a File", validators=[DataRequired()])
-    submit = SubmitField('Start Scoring')
-
 class ZDBFileUploadForm(FlaskForm):
     project_name = StringField("Enter Project Name", default=None)
     model = SelectField("Choose a Model", validators=[DataRequired()], validate_choice=False)
