4b578b0e80b2e58a01f1b38c7ad2a1fc6ec31e9b
MMAZeem3 <musa.mazeem@gmail.com>
2022-06-17 15:59:53 -0400

    clean webconfig file

--- lib/webconfig.py
+++ lib/webconfig.py
@@ -1,7 +1,18 @@
-FOLDERS = {'UPLOAD': 'from-client', 'DOWNLOAD': 'to-client', 'ARCHIVE': 'data-archive', 'GRAPHS': 'static/graphs'}
-DATA_DIRS = {'RAW': '0_raw', 'RAW_ZDB':'6_raw_zdb', 'FINAL': '5_final_lstm', 'FINAL_ZDB': '9_final_zdb_lstm', 'GRAPHS':'10_images'}
-ALLOWED_EXTENSIONS = {'ZIP':'.zip', 'XLS':'.xls', 'XLSX':'.xlsx', 'ZDB':'.zdb'}
-MODELS = {'LSTM Rat Model':'rat_lstm_WIN-9.h5', 'LSTM Mice Model':'mice_lstm_WIN-9.h5'}
 ADMIN_USERS = ['andrewsmith1025@gmail.com', 'musa.mazeem@gmail.com']
 MAIL_FROM = 'AuroraProjectEmail@gmail.com'
 MAIL_PASSWORD = 'kxfiusttkwlwneii'
+FOLDERS = {'UPLOAD': 'from-client', 
+           'DOWNLOAD': 'to-client', 
+           'ARCHIVE': 'data-archive', 
+           'GRAPHS': 'static/graphs'}
+DATA_DIRS = {'RAW': '0_raw', 
+             'RAW_ZDB':'6_raw_zdb', 
+             'FINAL': '5_final_lstm', 
+             'FINAL_ZDB': '9_final_zdb_lstm', 
+             'GRAPHS':'10_images'}
+ALLOWED_EXTENSIONS = {'ZIP':'.zip', 
+                      'XLS':'.xls', 
+                      'XLSX':'.xlsx', 
+                      'ZDB':'.zdb'}
+MODELS = {'LSTM Rat Model':'rat_lstm_WIN-9.h5', 
+          'LSTM Mice Model':'mice_lstm_WIN-9.h5'}
